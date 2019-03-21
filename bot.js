e2113bde80178de0cbf5ffb52552104fefbd0987
Pushpak <pushpak.kurella7@gmail.com>
2019-03-21 12:56:19 -0400

    added some simple replies

--- bot.js
+++ bot.js
@@ -2,12 +2,10 @@
 // Licensed under the MIT License.
 
 // bot.js is your bot's main entry point to handle incoming activities.
-
+const { CardFactory } = require('botbuilder');
 const { ActivityTypes } = require('botbuilder');
-
-// Turn counter property
-const TURN_COUNTER_PROPERTY = 'turnCounterProperty';
-
+const IntroCard = require('./resources/IntroCard.json');
+const WELCOMED_USER = 'welcomedUserProperty';
 class EchoBot {
     /**
      *
@@ -16,8 +14,9 @@ class EchoBot {
     constructor(conversationState) {
         // Creates a new state accessor property.
         // See https://aka.ms/about-bot-state-accessors to learn more about the bot state and state accessors
-        this.countProperty = conversationState.createProperty(TURN_COUNTER_PROPERTY);
+        //this.countProperty = conversationState.createProperty(TURN_COUNTER_PROPERTY);
         this.conversationState = conversationState;
+        this.welcomedUserProperty = conversationState.createProperty(WELCOMED_USER);
     }
     /**
      *
@@ -29,19 +28,71 @@ class EchoBot {
         // Handle message activity type. User's responses via text or speech or card interactions flow back to the bot as Message activity.
         // Message activities may contain text, speech, interactive cards, and binary or unknown attachments.
         // see https://aka.ms/about-bot-activity-message to learn more about the message and other activity types
+        
+
         if (turnContext.activity.type === ActivityTypes.Message) {
-            // read from state.
-            let count = await this.countProperty.get(turnContext);
-            count = count === undefined ? 1 : ++count;
-            await turnContext.sendActivity(`${ count }: Pushpak said "${ turnContext.activity.text }"`);
-            // increment and set turn counter.
-            await this.countProperty.set(turnContext, count);
-        } else {
+
+            const didBotWelcomedUser = await this.welcomedUserProperty.get(turnContext, false);
+
+            // Your bot should proactively send a welcome message to a personal chat the first time
+            // (and only the first time) a user initiates a personal chat with your bot.
+            if (didBotWelcomedUser === false) {
+                // The channel should send the user name in the 'From' object
+                //let userName = turnContext.activity.from.name;
+                await this.sendWelcomeMessage(turnContext);
+                // Set the flag indicating the bot handled the user's first message.
+
+                //await this.welcomedUserProperty.set(turnContext, true);
+            }
+            else {
+                let text = turnContext.activity.text.toLowerCase();
+                switch (text) {
+                case 'hello':
+                case 'hi':
+                    await turnContext.sendActivity(`Hello "${ turnContext.activity.text }" wass up how can i help you?`);
+                    break;
+                case 'intro':
+                case 'help':
+                    await turnContext.sendActivity({
+                         attachments: [CardFactory.adaptiveCard(IntroCard)]
+                    });
+                    break;
+                case 'location':
+                    await turnContext.sendActivity(`We are located at 25 Milling Road, Unit 303, Cambridge, ON`);
+                    break;
+                default :
+                    await turnContext.sendActivity(`This is a simple Welcome Bot sample. You can say 'intro' to
+                                                        see the introduction card. If you are running this bot in the Bot
+                                                        Framework Emulator, press the 'Start Over' button to simulate user joining a bot or a channel`);
+                }
+                
+            }
+            
+            
+        } else if(turnContext.activity.type === ActivityTypes.ConversationUpdate){
             // Generic handler for all other activity types.
-            await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
+            this.sendWelcomeMessage(turnContext);
+            await this.welcomedUserProperty.set(turnContext, true);
+        }
+        else{
+            // Generic message for all other activities
+            //await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
+        
         }
         // Save state changes
         await this.conversationState.saveChanges(turnContext);
+    }
+    async sendWelcomeMessage(turnContext){
+        //if we have new members added to the conversation
+        if(turnContext.activity.membersAdded.length !== 0){
+            for(let idx in turnContext.activity.membersAdded){
+                if(turnContext.activity.membersAdded[idx].id !== turnContext.activity.recipient.id){
+                    await turnContext.sendActivity(`Hi!!! my name is Qc Solver bot. 
+                    I am a simple prototype chatbot still learning things. You can type intro,help,location to know more about me!`);
+                    await turnContext.sendActivity(`what's your name?`);
+                }
+            }
+        }
     }
 }
 
